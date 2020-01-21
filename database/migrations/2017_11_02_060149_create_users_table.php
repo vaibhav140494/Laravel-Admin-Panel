f63a00ef0ac78c337d24e4cc4ed09d8992b6b803
unknown <nirav3998@gmail.com>
2020-01-21 15:07:12 +0530

    migration for users table  Changes to be committed: 	modified:   database/migrations/2017_11_02_060149_create_users_table.php

--- database/migrations/2017_11_02_060149_create_users_table.php
+++ database/migrations/2017_11_02_060149_create_users_table.php
@@ -14,15 +14,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name', 191);
-            $table->string('last_name', 191);
-            $table->string('email', 191)->unique();
-            $table->string('password', 191)->nullable();
+            $table->string('first_name', 100);
+            $table->string('last_name', 100 );
+            $table->string('address',250);
+            $table->integer('pincode');
+            $table->string('email',250)->unique();
+            $table->string('username',100);
+            $table->string('password',100)->nullable();
             $table->boolean('status')->default(1);
-            $table->string('confirmation_code', 191)->nullable();
+            $table->string('confirmation_code',191)->nullable();
             $table->boolean('confirmed')->default(0);
-            $table->boolean('is_term_accept')->default(0)->comment(' 0 = not accepted,1 = accepted');
-            $table->string('remember_token', 100)->nullable();
+            $table->boolean('is_term_accept')->default(0);
+            $table->string('phone_no',20);
+            $table->string('remember_token',100)->nullable();
+            $table->date('birthdate');
+            $table->boolean('is_verified')->default(0);
+            $table->boolean('role')->default(2);
             $table->integer('created_by')->unsigned();
             $table->integer('updated_by')->unsigned()->nullable();
             $table->timestamps();
